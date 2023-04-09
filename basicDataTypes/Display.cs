262606a7c65839b35f2ed2cd5869fdac73bc38fa
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-09 12:50:05 +0530

    [Ritesh]add : Practice Of Char data Type

---
+++ basicDataTypes/Display.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicDataTypes
+{
+    public class Display
+    {
+        public void Them()
+        {
+            char Letter ,Letter1,Letter2;
+            Console.WriteLine("Enter A Letter");
+            Letter = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter A Letter");
+            Letter1 = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter A Letter");
+            Letter2 = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("{0} {1} {2}",Letter2,Letter1,Letter);
+        }
+    }
+}
---
+++ basicDataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicDataTypes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 :Enter Three Lettter");
+            Console.WriteLine("Enter a Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    Display display = new Display();
+                    display.Them();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
