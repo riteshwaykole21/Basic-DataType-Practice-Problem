1f634686f8b0962557378855905aac44fdddceea
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-09 13:53:57 +0530

    [Ritesh]add : Area Of Circle Of Radius

--- basicDataTypes/Program.cs
+++ basicDataTypes/Program.cs
@@ -14,6 +14,7 @@ namespace basicDataTypes
             Console.WriteLine("1 :Enter Three Lettter");
             Console.WriteLine("2 : Display Trangular Of Width");
             Console.WriteLine("3 : Enter A Two Number");
+            Console.WriteLine("4 : Enter a Radius Of Circle");
             Console.WriteLine("Enter a Option");
             int Option = Convert.ToInt32(Console.ReadLine());
             switch(Option)
@@ -30,6 +31,11 @@ namespace basicDataTypes
                     Opration opration = new Opration();
                     opration.Taskes();
                     break;
+                case 4:
+                    RadiusCircle radiusCircle = new RadiusCircle();
+                    radiusCircle.Input();
+                    break;
+
             }
             Console.ReadLine();
         }
---
+++ basicDataTypes/RadiusCircle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicDataTypes
+{
+    public class RadiusCircle
+    {
+        public void Input()
+        {
+            Console.WriteLine("Enter The Radius Of Circle");
+            double Radius = Convert.ToDouble(Console.ReadLine());
+            double PI = 3.14;
+            double per_cir ;
+            per_cir = 2 * PI * Radius;
+            Console.WriteLine("Perimeter Of Circle : {0}" ,per_cir);
+
+
+        }
+    }
+}
